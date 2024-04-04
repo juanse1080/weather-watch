7d6d657f04baf74dd9318ce73fc1b2487f194bec
Juan Marcon <juanmarcon1080@gmail.com>
2024-04-04 10:15:33 -0500

    feat(api): add prisma to api

---
+++ src/app/lib/prisma.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@prisma/client";
+
+function initializePrisma() {
+  if (process.env.NODE_ENV === "production") {
+    return new PrismaClient();
+  } else {
+    if (!global.prisma) {
+      global.prisma = new PrismaClient();
+    }
+    return global.prisma;
+  }
+}
+
+const prisma = initializePrisma();
+
+export default prisma;
